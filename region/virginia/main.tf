549377056d809537443fca73089f628fec071158
natan-barel <natan.barel@outlook.co.il>
2023-07-06 17:24:51 +0300

    remove comments

--- region/virginia/main.tf
+++ region/virginia/main.tf
@@ -3,14 +3,12 @@ provider "aws" {
 }
 
 module "Networking" {
-  source             = "../../module/networking"
-  name               = "Terraform Project 1 VPC"
-  availability_zones = local.availability_zones
-  vpc_cidr_block     = "10.0.0.0/16"
-  # public_subnets_cidr_block = ["10.0.32.0/24", "10.0.96.0/24", "10.0.224.0/24"]
+  source                    = "../../module/networking"
+  name                      = "Terraform Project 1 VPC"
+  availability_zones        = local.availability_zones
+  vpc_cidr_block            = "10.0.0.0/16"
   public_subnets_cidr_block = ["10.0.0.0/24"]
-  # private_subnets_cidr_block = ["10.0.0.0/19", "10.0.64.0/19", "10.0.128.0/19"]
-  vpc_tags = local.tags
+  vpc_tags                  = local.tags
 }
 
 module "Jenkins" {
